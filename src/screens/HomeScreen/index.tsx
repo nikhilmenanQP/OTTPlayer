549a7691e30ab04856f7ddbf228638b83bea6e9b
nikhilmenanQP <nikhil.menan@ext.quickplay.com>
2024-12-03 11:46:21 +0530

    Fix: Use useCallBack for onMovieClickHandler and onProfileClickHandler in HomeScreen.tsx

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,7 +1,7 @@
 import AppCarousel from '@components/AppComponents/AppCarousel';
 import AppHeader from '@components/AppComponents/AppHeader';
 import MovieCard from '@components/HomeScreenComp/MovieCard';
-import React from 'react';
+import React, {useCallback} from 'react';
 import {NavigationProp} from '@react-navigation/native';
 import {FlatList, View} from 'react-native';
 import {createStyle} from './styles';
@@ -17,15 +17,18 @@ const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
   const {theme} = useAppTheme();
   const styles = createStyle(theme);
 
-  const onMovieClickHandler = (movie: any) => {
-    navigation.navigate('DetailScreen', {
-      data: movie,
-    });
-  };
+  const onMovieClickHandler = useCallback(
+    (movie: any) => {
+      navigation.navigate('DetailScreen', {
+        data: movie,
+      });
+    },
+    [navigation],
+  );
 
-  const onProfileClickHandler = () => {
+  const onProfileClickHandler = useCallback(() => {
     navigation.navigate('ProfileScreen');
-  };
+  }, [navigation]);
 
   const renderMovieCard = ({item}: {item: mapDataProps}) => {
     const {data, isContinueWatching, isWatchList, title} = item;
