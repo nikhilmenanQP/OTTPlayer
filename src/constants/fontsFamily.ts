ccde67477fbe88fd942ac0b0a61d55ba99ac7936
nikhilmenanQP <nikhil.menan@ext.quickplay.com>
2024-12-04 17:01:38 +0530

    Fix: Remove InterDisplay and InterVariable fonts

--- src/constants/fontsFamily.ts
+++ src/constants/fontsFamily.ts
@@ -3,26 +3,6 @@ interface FontFamily {
 }
 
 export const FONT_FAMILY: FontFamily = {
-  interDisplay_black: 'InterDisplay-Black',
-  interDisplay_blackItalic: 'InterDisplay-BlackItalic',
-  interDisplay_bold: 'InterDisplay-Bold',
-  interDisplay_boldItalic: 'InterDisplay-BoldItalic',
-  interDisplay_extraBold: 'InterDisplay-ExtraBold',
-  interDisplay_extraBoldItalic: 'InterDisplay-ExtraBoldItalic',
-  interDisplay_extraLight: 'InterDisplay-ExtraLight',
-  interDisplay_extraLightItalic: 'InterDisplay-ExtraLightItalic',
-  interDisplay_italic: 'InterDisplay-Italic',
-  interDisplay_light: 'InterDisplay-Light',
-  interDisplay_lightItalic: 'InterDisplay-LightItalic',
-  interDisplay_medium: 'InterDisplay-Medium',
-  interDisplay_mediumItalic: 'InterDisplay-MediumItalic',
-  interDisplay_regular: 'InterDisplay-Regular',
-  interDisplay_semiBold: 'InterDisplay-SemiBold',
-  interDisplay_semiBoldItalic: 'InterDisplay-SemiBoldItalic',
-  interDisplay_thin: 'InterDisplay-Thin',
-  interDisplay_thinItalic: 'InterDisplay-ThinItalic',
-  interVariable: 'InterVariable',
-  interVariable_italic: 'InterVariable-Italic',
   inter_Thin: 'Inter-Thin', // 100
   inter_ThinItalic: 'Inter-ThinItalic',
   inter_black: 'Inter-Black', // 900
