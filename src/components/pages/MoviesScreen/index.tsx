00a5da1a255cada9de4077bef14a07c8fddd0008
nikhilmenanQP <nikhil.menan@ext.quickplay.com>
2024-12-11 11:29:12 +0530

    MovieScreen: Atomic Design Breakdown

--- src/components/pages/MoviesScreen/index.tsx
+++ src/components/pages/MoviesScreen/index.tsx
@@ -1,51 +1,10 @@
-import React, {useMemo} from 'react';
-
-import {AppHeader} from '@components/molecules';
-import {FlatList, View} from 'react-native';
-import {MovieCardList} from '@components/organisms';
-
-import {createStyle} from './styles';
-import {mapDataProps, MoviesScreenProps} from './types';
+import MoviesScreenTemplate from '@components/templates/MovieScreenTemplate';
+import React from 'react';
+import {MoviesScreenProps} from './types';
 import {movieScreenData} from '../../../dummyDataPreProd/MoviesScreen';
-import {useAppTheme} from '@hooks/useAppTheme';
 
 const MoviesScreen: React.FC<MoviesScreenProps> = ({}) => {
-  const {theme} = useAppTheme();
-  const styles = useMemo(() => createStyle(theme), [theme]);
-
-  const renderMovieCardList = ({item}: {item: mapDataProps}) => (
-    <MovieCardList
-      data={item.data}
-      horizontalCard={item.isContinueWatching || item.isWatchList}
-      key={item.title}
-      marginLeft={false}
-      marginTop={false}
-      sectionContainerStyle={styles.sectionContainer}
-      showMovieDetails={item.isContinueWatching}
-      showTitle={true}
-      title={item.title}
-    />
-  );
-
-  return (
-    <View style={styles.container}>
-      {/* App Header */}
-      <AppHeader showBackButton={true} />
-
-      {/* FlatList to render MovieCardLists */}
-      <FlatList
-        contentContainerStyle={styles.contentContainerStyle}
-        data={movieScreenData}
-        initialNumToRender={5}
-        keyExtractor={item => item.title}
-        maxToRenderPerBatch={10}
-        removeClippedSubviews={true}
-        renderItem={renderMovieCardList}
-        showsVerticalScrollIndicator={false}
-        windowSize={10}
-      />
-    </View>
-  );
+  return <MoviesScreenTemplate data={movieScreenData} />;
 };
 
 export default MoviesScreen;
---
+++ src/components/templates/MovieScreenTemplate/index.tsx
@@ -0,0 +1,50 @@
+import React, {useMemo} from 'react';
+
+import {AppHeader} from '@components/molecules';
+import {FlatList, View} from 'react-native';
+import {MovieCardList} from '@components/organisms';
+
+import {createStyle} from './styles';
+import {mapDataProps, MoviesScreenProps} from './types';
+import {useAppTheme} from '@hooks/useAppTheme';
+
+const MoviesScreenTemplate: React.FC<MoviesScreenProps> = ({data}) => {
+  const {theme} = useAppTheme();
+  const styles = useMemo(() => createStyle(theme), [theme]);
+
+  const renderMovieCardList = ({item}: {item: mapDataProps}) => (
+    <MovieCardList
+      data={item.data}
+      horizontalCard={item.isContinueWatching || item.isWatchList}
+      key={item.title}
+      marginLeft={false}
+      marginTop={false}
+      sectionContainerStyle={styles.sectionContainer}
+      showMovieDetails={item.isContinueWatching}
+      showTitle={true}
+      title={item.title}
+    />
+  );
+
+  return (
+    <View style={styles.container}>
+      {/* App Header */}
+      <AppHeader showBackButton={true} />
+
+      {/* FlatList to render MovieCardLists */}
+      <FlatList
+        contentContainerStyle={styles.contentContainerStyle}
+        data={data}
+        initialNumToRender={5}
+        keyExtractor={item => item.title}
+        maxToRenderPerBatch={10}
+        removeClippedSubviews={true}
+        renderItem={renderMovieCardList}
+        showsVerticalScrollIndicator={false}
+        windowSize={10}
+      />
+    </View>
+  );
+};
+
+export default MoviesScreenTemplate;
