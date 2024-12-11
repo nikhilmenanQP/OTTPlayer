2001e53623749056f2dbaeb2e9b7066da55a6364
nikhilmenanQP <nikhil.menan@ext.quickplay.com>
2024-12-11 12:19:15 +0530

    SeriesScreen: Atomic Design Breakdown

--- src/components/pages/SeriesScreen/index.tsx
+++ src/components/pages/SeriesScreen/index.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
-import {AppHeader} from '@components/molecules';
-import {Danger} from '@assets/images/appIcons';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import {createStyle} from './styles';
-import {useAppTheme} from '@hooks/useAppTheme';
+import {StyleSheet} from 'react-native';
+import SeriesScreenTemplate from '@components/templates/SeriesScreenTemplate';
 
 export default function SeriesScreen() {
-  const {theme} = useAppTheme();
-  const styles = createStyle(theme);
-
-  return (
-    <>
-      <ScrollView contentContainerStyle={styles.scrollView} stickyHeaderIndices={[0]}>
-        <AppHeader showLogo={true} showMenuButton={true} />
-        <View style={styles.container}>
-          <View style={styles.notification}>
-            <Danger style={{marginBottom: theme.spacing.sm}} />
-            <Text style={styles.notificationText}>Series UI and Data is not available for now</Text>
-          </View>
-        </View>
-      </ScrollView>
-    </>
-  );
+  return <SeriesScreenTemplate />;
 }
 
 const styles = StyleSheet.create({});
